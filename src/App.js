f4ae7d1e5e1edff1350753c37a9237c7c3438923
Lucas Greenwell <lucasgreenwell15@gmail.com>
2019-12-20 10:50:23 -0600

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharList from './components/CharacterList'
+import WelcomePage from './components/WelcomePage';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <CharList/>
-    </main>
+    <Router>
+      <Header/>
+      
+      <Route exact path="/" component={WelcomePage}/>
+      <Route path="/" component={CharList}/>
+
+    </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to='/characters'>Characters</Link>
+      <Link to='/locations'>Locations</Link>
+      <Link to='/episodes'>Episodes</Link>
+      <Link to='/'>Home</Link>
     </header>
   );
 }
